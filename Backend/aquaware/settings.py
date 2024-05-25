ad10a1ad99f74626bbf2346b74ba51726d679ad5
kesslermatics <Robby9903@gmail.com>
2024-05-25 22:09:25 +0200

    Update settings.py

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
 
 DATABASES["default"] = dj_database_url.parse(os.getenv("DATABASE_URL"))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
