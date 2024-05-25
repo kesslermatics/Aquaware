63b69039c1d2a80b8ff966f371c537f6321f33b9
kesslermatics <Robby9903@gmail.com>
2024-05-25 22:49:40 +0200

    Update settings.py

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['aquaware-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['aquaware-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
