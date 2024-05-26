3fd72652a819924787cad14c714b2ecc91a2e222
kesslermatics <Robby9903@gmail.com>
2024-05-26 11:13:36 +0200

    Removed http

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -110,9 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
