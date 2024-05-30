ded60334994451fb2917c643c1e85243dbdf3da0
kesslermatics <Robby9903@gmail.com>
2024-05-30 12:55:25 +0200

    Update settings.py

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
