dd90e805324b332d93317c29df4ae180bf387d56
kesslermatics <Robby9903@gmail.com>
2024-05-30 20:23:21 +0200

    Added init app

---
+++ App/lib/screens/onboding/components/custom_sign_in.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'sign_in_form.dart';
+
+Future<Object?> customSigninDialog(BuildContext context,
+    {required ValueChanged onClosed}) {
+  return showGeneralDialog(
+      barrierDismissible: true,
+      barrierLabel: "Sign up",
+      context: context,
+      transitionDuration: const Duration(milliseconds: 400),
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        Tween<Offset> tween = Tween(begin: Offset(0, -1), end: Offset.zero);
+        return SlideTransition(
+            position: tween.animate(
+                CurvedAnimation(parent: animation, curve: Curves.easeInOut)),
+            child: child);
+      },
+      pageBuilder: (context, _, __) => Center(
+            child: Container(
+              height: 620,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.95),
+                  borderRadius: const BorderRadius.all(Radius.circular(40))),
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                resizeToAvoidBottomInset:
+                    false, // avoid overflow error when keyboard shows up
+                body: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Column(children: [
+                      const Text(
+                        "Sign In",
+                        style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          "Access to 240+ hours of content. Learn design and code, by builder real apps with Flutter and Swift.",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SignInForm(),
+                    ]),
+                    const Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: -48,
+                      child: CircleAvatar(
+                        radius: 16,
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.close, color: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )).then(onClosed);
+}
