ca6b8c035264e7244911d1c29a45fe57fcc8d1d1
kesslermatics <Robby9903@gmail.com>
2024-05-30 12:07:33 +0200

    Update settings.py

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -115,6 +115,10 @@ REST_FRAMEWORK = {
     ],
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://aquaware-production.up.railway.app'
+]
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
