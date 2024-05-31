61f80b8cd71469bc6c37972ae5122a9a3c9b12c4
kesslermatics <Robby9903@gmail.com>
2024-05-31 19:21:31 +0200

    Added logout button

--- App/lib/screens/homepage/homepage.dart
+++ App/lib/screens/homepage/homepage.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomepageScreen extends StatelessWidget {
+  const HomepageScreen({super.key});
+
+  Future<void> _logout(BuildContext context) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('accessToken');
+    await prefs.remove('refreshToken');
+    Navigator.pushReplacementNamed(context, '/onboarding');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Homepage'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
       ),
       body: Center(
         child: Text('Welcome to the Homepage!'),
