ceef2390495586fc03afa94bc87f9f1e237706af
kesslermatics <Robby9903@gmail.com>
2024-05-31 17:57:47 +0200

    Update settings.py

--- Backend/aquaware/settings.py
+++ Backend/aquaware/settings.py
@@ -50,7 +50,6 @@ STATIC_URL = "static/"
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
