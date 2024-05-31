5a1f74bf713e700c681e1946b1832e3a667d4b1b
kesslermatics <Robby9903@gmail.com>
2024-05-31 19:14:04 +0200

    Added jwt remembering

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:aquaware/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'screens/homepage/homepage.dart';
 import 'screens/onboarding/onboding_screen.dart';
@@ -7,17 +11,69 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoading = true;
+  bool _isAuthenticated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthentication();
+  }
+
+  Future<void> _checkAuthentication() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? accessToken = prefs.getString('accessToken');
+
+    if (accessToken != null && accessToken.isNotEmpty) {
+      try {
+        final response = await http.get(
+          Uri.parse('$baseURL/user/jwttest/'), // Replace with your base URL
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer $accessToken',
+          },
+        );
+
+        if (response.statusCode == 200) {
+          setState(() {
+            _isAuthenticated = true;
+          });
+        } else {
+          setState(() {
+            _isAuthenticated = false;
+          });
+        }
+      } catch (e) {
+        setState(() {
+          _isAuthenticated = false;
+        });
+      }
+    } else {
+      setState(() {
+        _isAuthenticated = false;
+      });
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Aquaware',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFEEF1F8),
+        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
         primarySwatch: Colors.blue,
         fontFamily: "Intel",
         inputDecorationTheme: const InputDecorationTheme(
@@ -30,9 +86,16 @@ class MyApp extends StatelessWidget {
           errorBorder: defaultInputBorder,
         ),
       ),
-      initialRoute: '/',
+      home: _isLoading
+          ? Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : _isAuthenticated
+              ? HomepageScreen()
+              : const OnboardingScreen(),
       routes: {
-        '/': (context) => OnboardingScreen(),
         '/homepage': (context) => HomepageScreen(),
       },
     );
