092ee82173e1ccdb0b141e8e648894ff65713565
kesslermatics <Robby9903@gmail.com>
2024-07-23 15:21:55 +0200

    Added card to show details

--- App/lib/screens/dashboard/water_parameter_card.dart
+++ App/lib/screens/dashboard/water_parameter_card.dart
@@ -1,76 +1,170 @@
-import 'package:aquaware/models/water_value.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:aquaware/models/water_parameter.dart';
-import 'package:intl/intl.dart';
 
-class WaterParameterCard extends StatelessWidget {
+class WaterParameterCard extends StatefulWidget {
   final WaterParameter waterParameter;
 
   WaterParameterCard({required this.waterParameter});
 
   @override
-  Widget build(BuildContext context) {
-    final latestValue = waterParameter.values.first;
+  _WaterParameterCardState createState() => _WaterParameterCardState();
+}
 
+class _WaterParameterCardState extends State<WaterParameterCard> {
+  late List<Color> gradientColors;
+  late List<double> data;
+  late List<String> dates;
+  late double deviation;
+  late double yInterval;
+  late double xInterval;
+
+  @override
+  void initState() {
+    super.initState();
+    gradientColors = [Colors.blue, Colors.blueAccent];
+    data = widget.waterParameter.values.map((e) => e.value).toList();
+    dates = widget.waterParameter.values
+        .map((e) => e.measuredAt.toIso8601String())
+        .toList();
+    deviation = (data.reduce((a, b) => a > b ? a : b) -
+            data.reduce((a, b) => a < b ? a : b)) /
+        10;
+    yInterval = deviation / 2;
+    xInterval = 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final latestValue = widget.waterParameter.values.first;
     return Card(
-      margin: EdgeInsets.all(8),
+      margin: EdgeInsets.fromLTRB(14, 10, 14, 0),
       color: Colors.white12,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              waterParameter.parameter,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Row(
-              children: [
-                Text(
-                  '${latestValue.value} ${latestValue.unit}',
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(width: 16),
-                Expanded(
-                  child: SizedBox(
-                    height: 100,
-                    child: SfCartesianChart(
-                      primaryXAxis: DateTimeAxis(
-                        dateFormat: DateFormat.Md(),
-                        intervalType: DateTimeIntervalType.auto,
-                        majorGridLines: MajorGridLines(width: 0),
-                      ),
-                      primaryYAxis: NumericAxis(
-                        labelFormat: '{value} ${latestValue.unit}',
-                        majorGridLines: MajorGridLines(width: 0),
-                      ),
-                      series: <CartesianSeries>[
-                        LineSeries<WaterValue, DateTime>(
-                          dataSource: waterParameter.values,
-                          xValueMapper: (WaterValue value, _) =>
-                              value.measuredAt,
-                          yValueMapper: (WaterValue value, _) => value.value,
-                          name: waterParameter.parameter,
-                          color: Color.fromRGBO(8, 142, 255, 1),
-                          markerSettings: MarkerSettings(
-                            isVisible: true, // Display markers at data points
-                            shape: DataMarkerType.circle, // Shape of the marker
-                            borderWidth: 2,
-                            borderColor: Color.fromRGBO(8, 142, 255, 1),
-                          ),
+      child: IntrinsicHeight(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 14, 0),
+                child: Container(
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _getIconForParameter(widget.waterParameter.parameter),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                        child: Text(
+                          '${widget.waterParameter.parameter} (${latestValue.unit})',
+                          textAlign: TextAlign.center,
                         ),
-                      ],
-                      tooltipBehavior: TooltipBehavior(enable: true),
-                    ),
+                      ),
+                      AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 500),
+                        transitionBuilder:
+                            (Widget child, Animation<double> animation) {
+                          return ScaleTransition(
+                              scale: animation, child: child);
+                        },
+                        child: Text(
+                          latestValue.value.toString(),
+                          key: ValueKey<int>(
+                            latestValue.value.round(),
+                          ),
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: VerticalDivider(
+                  color: _getColorForParameter(widget.waterParameter.parameter),
+                  thickness: 2,
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 60,
+                  child: LineChart(mainData()),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  LineChartData mainData() {
+    List<FlSpot> spots = [];
+    for (int i = 0; i < data.length; i++) {
+      spots.add(FlSpot(double.parse((i + 1).toString()), data[i]));
+    }
+
+    return LineChartData(
+      clipData: FlClipData.all(),
+      gridData: FlGridData(
+        show: false,
+      ),
+      minY: data.reduce((a, b) => a < b ? a : b) - deviation,
+      maxY: data.reduce((a, b) => a > b ? a : b) + deviation,
+      minX: 1,
+      maxX: data.length.toDouble(),
+      titlesData: FlTitlesData(
+        show: false,
+      ),
+      borderData: FlBorderData(
+        show: false,
+      ),
+      lineBarsData: [
+        LineChartBarData(
+          spots: spots,
+          isCurved: true,
+          color: _getColorForParameter(widget.waterParameter.parameter),
+          barWidth: 3,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: false,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getIconForParameter(String parameter) {
+    switch (parameter.toLowerCase()) {
+      case 'temperature':
+        return Icon(Icons.thermostat, color: Colors.red, size: 40);
+      case 'ph':
+        return Icon(Icons.opacity, color: Colors.blue, size: 40);
+      case 'tds':
+        return Icon(Icons.invert_colors, color: Colors.brown, size: 40);
+      case 'chlorophyll-a':
+        return Icon(Icons.grass, color: Colors.green, size: 40);
+      default:
+        return Icon(Icons.device_thermostat, color: Colors.grey, size: 40);
+    }
+  }
+
+  Color _getColorForParameter(String parameter) {
+    switch (parameter.toLowerCase()) {
+      case 'temperature':
+        return Colors.red;
+      case 'ph':
+        return Colors.blue;
+      case 'tds':
+        return Colors.brown;
+      case 'chlorophyll-a':
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
   }
 }
