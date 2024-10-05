51bf8fd674b0ad57482e9e76469c1aff04bc5b2b
kesslermatics <Robby9903@gmail.com>
2024-10-06 00:24:46 +0200

    added paypal button

--- Website/src/components/Dashboard/PricingPlanInfo.jsx
+++ Website/src/components/Dashboard/PricingPlanInfo.jsx
@@ -1,26 +1,75 @@
 import { check } from "../../assets";
 import { pricing } from "../../constants";
-import Button from "../Button";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Cookies from "js-cookie";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const PricingPlanInfo = () => {
   const [userPlan, setUserPlan] = useState(null);
   const [selectedPlan, setSelectedPlan] = useState(null);
   const [message, setMessage] = useState("");
+  const paypalRef = useRef(null);
+
+  // Function to refresh the access token
+  const refreshAccessToken = async () => {
+    try {
+      const refreshToken = Cookies.get("refresh_token");
+      const response = await fetch(
+        "https://dev.aquaware.cloud/api/users/token/refresh/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refresh: refreshToken }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        Cookies.set("access_token", data.access);
+        return data.access;
+      } else {
+        throw new Error("Failed to refresh token");
+      }
+    } catch (error) {
+      console.error("Error refreshing access token:", error);
+      throw error;
+    }
+  };
+
+  // Function to handle API requests with automatic token refresh
+  const fetchWithTokenRefresh = async (url, options) => {
+    try {
+      let accessToken = Cookies.get("access_token");
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${accessToken}`,
+      };
+
+      let response = await fetch(url, options);
+
+      // If the token has expired, refresh it and retry the request
+      if (response.status === 401) {
+        accessToken = await refreshAccessToken();
+        options.headers.Authorization = `Bearer ${accessToken}`;
+        response = await fetch(url, options);
+      }
+
+      return response;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchUserPlan = async () => {
       try {
-        const accessToken = Cookies.get("access_token");
-        const response = await fetch(
+        const response = await fetchWithTokenRefresh(
           "https://dev.aquaware.cloud/api/users/profile/",
           {
             method: "GET",
             headers: {
               "Content-Type": "application/json",
-              Authorization: `Bearer ${accessToken}`,
             },
             credentials: "include",
           }
@@ -37,6 +86,102 @@ const PricingPlanInfo = () => {
 
     fetchUserPlan();
   }, []);
+
+  useEffect(() => {
+    if (selectedPlan && window.paypal) {
+      paypalRef.current.innerHTML = ""; // Clear PayPal button container before rendering
+
+      window.paypal
+        .Buttons({
+          style: {
+            shape: "rect",
+            layout: "vertical",
+            color: "gold",
+            label: "paypal",
+          },
+          message: {
+            amount: selectedPlan.price,
+          },
+          async createOrder() {
+            try {
+              const response = await fetchWithTokenRefresh(
+                "https://dev.aquaware.cloud/api/orders/create-order/",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    plan: selectedPlan.id,
+                    amount: 1,
+                  }),
+                }
+              );
+
+              const orderData = await response.json();
+              console.log("Order data:", orderData.id);
+              if (orderData.id) {
+                return orderData.id;
+              } else {
+                const errorDetail = orderData?.details?.[0];
+                const errorMessage = errorDetail
+                  ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                  : JSON.stringify(orderData);
+
+                throw new Error(errorMessage);
+              }
+            } catch (error) {
+              console.error(error);
+              setMessage(`Could not initiate PayPal Checkout...${error}`);
+            }
+          },
+          async onApprove(data, actions) {
+            try {
+              const response = await fetchWithTokenRefresh(
+                `https://dev.aquaware.cloud/api/orders/${data.orderID}/capture/`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+
+              const orderData = await response.json();
+              const errorDetail = orderData?.details?.[0];
+
+              if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                return actions.restart();
+              } else if (errorDetail) {
+                throw new Error(
+                  `${errorDetail.description} (${orderData.debug_id})`
+                );
+              } else if (!orderData.purchase_units) {
+                throw new Error(JSON.stringify(orderData));
+              } else {
+                const transaction =
+                  orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
+                  orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
+                setMessage(
+                  `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
+                );
+                console.log(
+                  "Capture result",
+                  orderData,
+                  JSON.stringify(orderData, null, 2)
+                );
+              }
+            } catch (error) {
+              console.error(error);
+              setMessage(
+                `Sorry, your transaction could not be processed...${error}`
+              );
+            }
+          },
+        })
+        .render(paypalRef.current);
+    }
+  }, [selectedPlan]);
 
   const handlePlanClick = (plan) => {
     if (plan.id !== userPlan) {
@@ -57,21 +202,13 @@ const PricingPlanInfo = () => {
   const getButtonClass = (planId) => {
     if (planId == userPlan) {
       return "bg-green-500 text-white cursor-default";
-    } else if (planId > userPlan) {
-      return "bg-blue-500 text-white hover:bg-blue-600"; // Upgrade button
-    } else if (planId < userPlan) {
-      return "bg-blue-500 text-white hover:bg-blue-600"; // Downgrade button
+    } else if (planId > userPlan || planId < userPlan) {
+      return "bg-blue-500 text-white hover:bg-blue-600"; // Upgrade or Downgrade button
     }
   };
 
   const isButtonDisabled = (planId) => {
     return planId == userPlan; // Disable button if it's the current plan
-  };
-
-  const initialOptions = {
-    "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
-    currency: "EUR",
-    components: "buttons",
   };
 
   return (
@@ -123,7 +260,7 @@ const PricingPlanInfo = () => {
         ))}
       </div>
 
-      {/* Display selected plan details */}
+      {/* Display selected plan details and PayPal button */}
       {selectedPlan && (
         <div className="mt-8 w-full max-w-7xl mx-auto p-6 border border-n-6 rounded-xl shadow-lg">
           <h3 className="text-xl font-semibold mb-4">Selected Plan</h3>
@@ -137,99 +274,12 @@ const PricingPlanInfo = () => {
             <strong>Description:</strong> {selectedPlan.description}
           </p>
 
-          <div className="mt-4">
-            <PayPalScriptProvider options={initialOptions}>
-              <PayPalButtons
-                style={{
-                  shape: "rect",
-                  layout: "vertical",
-                  color: "gold",
-                  label: "paypal",
-                }}
-                createOrder={async () => {
-                  try {
-                    const response = await fetch(
-                      "https://dev.aquaware.cloud/api/orders",
-                      {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                          cart: [
-                            {
-                              id: selectedPlan.id,
-                              quantity: 1,
-                            },
-                          ],
-                        }),
-                      }
-                    );
-
-                    const orderData = await response.json();
-
-                    if (orderData.id) {
-                      return orderData.id;
-                    } else {
-                      const errorDetail = orderData?.details?.[0];
-                      const errorMessage = errorDetail
-                        ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
-                        : JSON.stringify(orderData);
-
-                      throw new Error(errorMessage);
-                    }
-                  } catch (error) {
-                    console.error(error);
-                    setMessage(`Could not initiate PayPal Checkout...${error}`);
-                  }
-                }}
-                onApprove={async (data, actions) => {
-                  try {
-                    const response = await fetch(
-                      `https://dev.aquaware.cloud/api/orders/${data.orderID}/capture`,
-                      {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                          plan: selectedPlan.id,
-                        }),
-                      }
-                    );
-
-                    const orderData = await response.json();
-                    const errorDetail = orderData?.details?.[0];
-
-                    if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-                      return actions.restart();
-                    } else if (errorDetail) {
-                      throw new Error(
-                        `${errorDetail.description} (${orderData.debug_id})`
-                      );
-                    } else {
-                      const transaction =
-                        orderData.purchase_units[0].payments.captures[0];
-                      setMessage(
-                        `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
-                      );
-                      console.log(
-                        "Capture result",
-                        orderData,
-                        JSON.stringify(orderData, null, 2)
-                      );
-                    }
-                  } catch (error) {
-                    console.error(error);
-                    setMessage(
-                      `Sorry, your transaction could not be processed...${error}`
-                    );
-                  }
-                }}
-              />
-            </PayPalScriptProvider>
-            <p>{message}</p>
-          </div>
+          <div
+            className="mt-4"
+            id="paypal-button-container"
+            ref={paypalRef}
+          ></div>
+          <p>{message}</p>
         </div>
       )}
     </div>
