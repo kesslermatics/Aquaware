fc9a2905c641278bb842ccc513f2cb71ff1ca1b9
kesslermatics <Robby9903@gmail.com>
2024-12-04 20:47:04 +0100

    Update Header.jsx

--- Website/src/components/Header.jsx
+++ Website/src/components/Header.jsx
@@ -107,11 +107,7 @@ const Header = () => {
               target="_blank"
               rel="noreferrer noopener"
               onClick={handleClick}
-              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                pathname === "/docs/index.html"
-                  ? "z-2 lg:text-n-1"
-                  : "lg:text-n-1/50"
-              } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              className={`block relative font-code text-2xl uppercase text-n-1/50 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold  lg:leading-5 lg:hover:text-n-1 xl:px-12`}
             >
               {t("header.links.github")}
             </a>
