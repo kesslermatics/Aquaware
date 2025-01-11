0ff9536521fccc089e17863ee5c3f75aa04f0c47
kesslermatics <Robby9903@gmail.com>
2025-01-11 13:19:06 +0100

    Update animal_detection_screen.dart

--- App/lib/screens/animal_detection/animal_detection_screen.dart
+++ App/lib/screens/animal_detection/animal_detection_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
 import 'dart:io';
+import 'package:aquaware/models/user_profile.dart';
 import 'package:aquaware/screens/animal_detection/animal_result_screen.dart';
 import 'package:aquaware/services/animal_detection_service.dart';
+import 'package:aquaware/services/color_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AnimalDetectionScreen extends StatefulWidget {
@@ -15,10 +19,25 @@ class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
   File? _selectedImage;
   String? _errorMessage;
   bool _isLoading = false;
+  bool isLocked = false;
 
   final ImagePicker _picker = ImagePicker();
 
-  // Method to pick image from gallery
+  @override
+  void initState() {
+    super.initState();
+    _checkUserSubscription();
+  }
+
+  void _checkUserSubscription() {
+    final userProfile = UserProfile.getInstance();
+    if (userProfile.subscriptionTier == 1) {
+      setState(() {
+        isLocked = true;
+      });
+    }
+  }
+
   Future<void> _pickImage() async {
     try {
       final pickedFile = await _picker.pickImage(
@@ -36,7 +55,6 @@ class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
     }
   }
 
-  // Method to capture image from camera
   Future<void> _captureImage() async {
     try {
       final pickedFile = await _picker.pickImage(
@@ -54,7 +72,6 @@ class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
     }
   }
 
-  // Method to analyze the image using FishDetectionService
   Future<void> _analyzeImage() async {
     if (_selectedImage == null) {
       setState(() {
@@ -67,6 +84,14 @@ class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
       _isLoading = true;
       _errorMessage = null;
     });
+
+    Get.snackbar(
+      "Analyzing Image",
+      "Please wait while we analyze the image.",
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: ColorProvider.n6,
+      colorText: Colors.white,
+    );
 
     try {
       final service = AnimalDetectionService();
@@ -96,68 +121,126 @@ class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const Text(
-              'Upload or capture an image of the aquatic animal to identify its species.',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: _pickImage,
-              child: Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: _selectedImage != null
-                    ? Image.file(_selectedImage!, fit: BoxFit.cover)
-                    : const Center(
-                        child: Icon(Icons.camera_alt,
-                            size: 50, color: Colors.grey),
-                      ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                ElevatedButton.icon(
-                  onPressed: _pickImage,
-                  icon: const Icon(Icons.upload_file),
-                  label: const Text('Upload Image'),
+                const Text(
+                  'Upload or capture an image of the aquatic animal to identify its species.',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
                 ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: _selectedImage != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.file(
+                              _selectedImage!,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        : const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.camera_alt,
+                                  size: 50, color: Colors.grey),
+                              SizedBox(height: 10),
+                              Text(
+                                "Upload Image",
+                                style:
+                                    TextStyle(fontSize: 16, color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Row(
+                  children: [
+                    Expanded(child: Divider()),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text("OR"),
+                    ),
+                    Expanded(child: Divider()),
+                  ],
+                ),
+                const SizedBox(height: 20),
                 ElevatedButton.icon(
                   onPressed: _captureImage,
                   icon: const Icon(Icons.camera_alt),
                   label: const Text('Capture Image'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                 ),
+                const SizedBox(height: 20),
+                _isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : ElevatedButton(
+                        onPressed: _analyzeImage,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                        ),
+                        child: const Text('Analyze'),
+                      ),
+                if (_errorMessage != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Text(
+                      _errorMessage!,
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                  ),
               ],
             ),
-            const SizedBox(height: 20),
-            _isLoading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: _analyzeImage,
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(double.infinity, 50),
+          ),
+          if (isLocked)
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: const Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.lock,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'This feature is available only in the Advanced or Business Plan.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
                     ),
-                    child: const Text('Analyze'),
                   ),
-            if (_errorMessage != null)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  _errorMessage!,
-                  style: const TextStyle(color: Colors.red),
                 ),
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
