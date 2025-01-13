7b3dc74860e1fe75f28e176736f130d3f9c0029a
kesslermatics <Robby9903@gmail.com>
2025-01-13 11:31:23 +0100

    Update views.py

--- Backend/disease/views.py
+++ Backend/disease/views.py
@@ -45,7 +45,7 @@ def diagnosis_from_image(request):
             f"You will receive an image of an aquatic animal. Your task is to carefully and thoroughly determine whether the animal has any disease. "
             f"Only provide a diagnosis if you are highly certain of the condition. In case of doubt, carefully review every detail before making a judgment. "
             f"If there is any uncertainty, increase your attention to the smallest visual cues of disease. "
-            f"Respond **only** in the specified language '{language}' and only in this language for the values and exclusively in the following JSON format **without any additional text or formatting or explanations as clear text** and start with uppercase in these JSON values:\n"
+            f"Respond **ONLY** in the specified language '{language}' AND ONLY IN THIS LANGUAGE for the json values and exclusively in the following JSON format **without any additional text or formatting or explanations as clear text** and start with uppercase in these JSON values:\n"
             f"{{\n"
             f'  "animal_detected": true or false,\n'
             f'  "condition": "Healthy" or the identified disease with their name,\n'
