64731f8ad739bff9188fc8604e42c7ce2d61d2da
Robert Kessler <robby9903@gmail.com>
2025-03-08 09:58:31 +0100

    Added docs

--- Website/docs/src/pages/index.js
+++ Website/docs/src/pages/index.js
@@ -1,36 +1,30 @@
 import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-
-import Heading from "@theme/Heading";
 import styles from "./index.module.css";
-import Translate, { translate } from "@docusaurus/Translate";
+import React from "react";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+function LanguageSelection() {
   return (
     <header className={clsx("", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          <Translate>
-            Aquaware API Documentation
-          </Translate>
-        </Heading>
-        <p className="hero__subtitle">
-          <Translate>
-            Enjoy the easy to read documentation to get started quickly
-          </Translate>
-        </p>
-        <div className={styles.buttons}>
-          <Link
+      <div className={clsx(styles.languageSelection, "container")}> 
+        <h1 className="hero__title" style={{ fontSize: "2.5rem" }}>Welcome to Aquaware</h1>
+        <p className="hero__subtitle" style={{ fontSize: "1.5rem" }}>Select your preferred language</p>
+        <div className={styles.buttons} style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          <a
             className="button button--secondary button--lg"
-            to="/docs/getting-started/welcome/"
+            href="https://docs.aquaware.cloud/docs/getting-started/welcome"
+            style={{ display: "flex", alignItems: "center", fontSize: "1.25rem", padding: "15px 25px" }}
           >
-            <Translate>
-              Click here to get started
-            </Translate>
-          </Link>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg" alt="English" style={{ width: 32, height: 22, marginRight: 15, verticalAlign: "middle" }} />
+            English
+          </a>
+          <a
+            className="button button--secondary button--lg"
+            href="https://docs.aquaware.cloud/de/docs/getting-started/welcome"
+            style={{ display: "flex", alignItems: "center", fontSize: "1.25rem", padding: "15px 25px" }}
+          >
+            <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg" alt="Deutsch" style={{ width: 32, height: 22, marginRight: 15, verticalAlign: "middle" }} />
+            Deutsch
+          </a>
         </div>
       </div>
     </header>
@@ -38,14 +32,5 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main></main>
-    </Layout>
-  );
+  return <LanguageSelection />;
 }
